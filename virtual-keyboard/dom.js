1a1d120ef5e63a902280d6ca0447ef93c2f125a0
Sergei Klimov <sergei_klimov_1998@mail.ru>
2020-12-03 14:29:26 +0300

    fix: add correct functions

--- virtual-keyboard/dom.js
+++ virtual-keyboard/dom.js
@@ -5,27 +5,33 @@ const symbols = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace", 
  "t", "y", "u", "i", "o", "p", "\[", "\]", "caps", "a", "s", "d","f", "g", "h", "j", "k", "l", ";", 
  "\'", "enter", "ok", "z", "x", "c", "v", "b", "n", "m", ",", ".", "\(","\)","?", "space"];
 
-let isUpperCase = false;
+ let isUpperCase = false;
 let layout = new Array();
 
 // Create array of HTML elements
 for (let i = 0; i < symbols.length + 4; i++) {
-  let item = document.createElement('button');
+  let item;
   switch(i) {
-    case 11: item = document.createElement('br');
-                    layout.push(item);
-    break;
-    case 24: item = document.createElement('br');
-                    layout.push(item);
-    break;
-    case 38: item = document.createElement('br');
-                    layout.push(item);
-    break;
-    case 52: item = document.createElement('br');
-                    layout.push(item);
-    break;
-    default: layout.push(item);
-    break;
+    case 11: 
+      item = document.createElement('br');
+      layout.push(item);
+      break;
+    case 24: 
+      item = document.createElement('br');
+      layout.push(item);
+      break;
+    case 38: 
+      item = document.createElement('br');
+      layout.push(item);
+      break;
+    case 52: 
+      item = document.createElement('br');
+      layout.push(item);
+      break;
+    default: 
+      item  = document.createElement('button');
+      layout.push(item);
+      break;
   };
 };
 
@@ -35,37 +41,42 @@ layout.forEach(item => fragment.appendChild(item));
 const keysArray = fragment.querySelectorAll('button');
 
 // Style keys
-keysArray.forEach(function(item, index) {
+keysArray.forEach((item, index) => {
   item.textContent = symbols[index];
-  item.classList.add('key');
   switch(item.textContent) {
-    case 'space': item.textContent = '';
-                  item.classList.add('key_space');
-    break;
-    case 'backspace': item.textContent = ''; 
-                      item.classList.add('key_backspace');
-    break;
-    case 'enter': item.textContent = ''; 
-                      item.classList.add('key_enter');
-    break;
-    case 'caps': item.textContent = ''; 
-                  item.classList.add('key_caps');
-    break;
-    case 'ok': item.textContent = ''; 
-                  item.classList.add('key_ok');
-    break;
-    case 'enter': item.textContent = ''; 
-                  item.classList.add('key_enter');
-    break;
+    case 'space': 
+      item.textContent = '';
+      item.className = 'key key_space';
+      break;
+    case 'backspace': 
+      item.textContent = ''; 
+      item.className = 'key key_backspace';
+      break;
+    case 'enter': 
+      item.textContent = ''; 
+      item.className = 'key key_enter';
+      break;
+    case 'caps': 
+      item.textContent = ''; 
+      item.className = 'key key_caps';
+      break;
+    case 'ok': 
+      item.textContent = ''; 
+      item.className = 'key key_ok';
+      break;
+    case 'enter': 
+      item.textContent = ''; 
+      item.className = 'key key_enter';
+      break;
+    default:
+      item.classList.add('key');
+      break;
   };
 });
 
 // On/Off Caps Lock
 function toggleCase(element) {
-  if (isUpperCase) {
-    isUpperCase = false;
-  }
-  else isUpperCase = true;
+  isUpperCase = !isUpperCase;
   element.classList.toggle('active');
   keysArray.forEach(key => key.classList.toggle('upper'));
 };
@@ -78,10 +89,7 @@ function handleMouse(e) {
     keyBoard.classList.add('hidden');
   };
   if (e.target.className === 'key' || e.target.className === 'key upper') {
-    if (isUpperCase) {
-      text.textContent += e.target.textContent.toUpperCase();
-    }
-    else text.textContent += e.target.textContent;
+    text.textContent += `${isUpperCase ? e.target.textContent.toUpperCase() : e.target.textContent}`;
   };
   if (e.target.className.includes('key_backspace')) {
     text.textContent = text.textContent.substring(0, text.textContent.length - 1);
@@ -107,7 +115,5 @@ function highlightKey(e) {
 // Insert keys in Keyboard
 keyBoard.appendChild(fragment);
 
-window.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('click', handleMouse);
-  window.addEventListener('mousedown', highlightKey);
-});
+window.addEventListener('click', handleMouse);
+window.addEventListener('mousedown', highlightKey);
