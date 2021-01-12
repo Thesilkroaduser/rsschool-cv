069edda7b97326f55032a83e62ba9a5d2b650b46
Sergei Klimov <sergei_klimov_1998@mail.ru>
2021-01-12 16:23:12 +0300

    fix: add correct vars

--- fancyweather/src/components/map.js
+++ fancyweather/src/components/map.js
@@ -3,15 +3,10 @@ import ReactMapGL from 'react-map-gl';
 
 function MapArea(data) {
   const { mapSettings } = data.mapSettings;
-  // State for Coords
   const coords = {
     lat: mapSettings.latitude,
     lng: mapSettings.longitude,
-    minsLt: 0,
-    minsLg: 0,
   };
-  // State for Map
-  // const [viewport, setViewport] = useState(mapSettings);
   const viewport = mapSettings;
 
   return (
@@ -23,26 +18,23 @@ function MapArea(data) {
           {...viewport}
           mapboxApiAccessToken="pk.eyJ1Ijoic2VyZ2VpZGV2IiwiYSI6ImNramlpZjlxMzE5dmEyc2xvcjJ2czg2OGYifQ.75myhpcDhNYozJjZXFmsVg"
           mapStyle="mapbox://styles/sergeidev/ckjiiqeehfnis19o53a59pdmy"
-          // onViewportChange={(view) => {
-          //   setViewport(view);
-          // }}
         />
       </div>
       <div className="coords">
         <p className="coords__item">
           latitude:
           {' '}
-          {coords.lat}
+          {Math.trunc(coords.lat)}
           &deg;
-          {coords.minsLt}
+          {Math.trunc((coords.lat % 1) * 100)}
           `
         </p>
         <p className="coords__item">
           longitude:
           {' '}
-          {coords.lng}
+          {Math.trunc(coords.lng)}
           &deg;
-          {coords.minsLg}
+          {Math.trunc((coords.lng % 1) * 100)}
           `
         </p>
       </div>
